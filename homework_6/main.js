1f6e5df587de9e6135c00e7848bab265d29b4c44
Nawon Choi <nawonc@andrew.cmu.edu>
2018-10-26 14:57:34 -0400

    good

--- homework_6/main.js
+++ homework_6/main.js
@@ -4,8 +4,8 @@ function CinnamonRoll(flavor, glaze, quantity) {
   this.flavor = flavor;
   this.glaze = glaze;
   this.quantity = quantity;
-  this.price = '3.99'; 
-  this.totalPrice = ('3.99'*quantity).toString();
+  this.price = '1.99'; 
+  this.totalPrice = (this.price * quantity).toString();
   if (this.glaze == 'None') {
     this.image = flavor + ".png";
   } else if (this.glaze == 'Sugar-milk') {
@@ -15,81 +15,143 @@ function CinnamonRoll(flavor, glaze, quantity) {
   } else {
     this.image = flavor + '-choco.png';
   };
+  if (this.flavor == 'original') {
+    this.rollName = "Original";
+  } else if (this.flavor == 'blackberry') {
+    this.rollName = "Blackberry";
+  } else if (this.flavor == 'walnut') {
+    this.rollName = "Walnut";
+  } else if (this.flavor == 'gluten') {
+    this.rollName = "Original (Gluten-free)";
+  } else if (this.flavor == 'pumpkin') {
+    this.rollName = "Pumpkin Spice";
+  } else {
+    this.rollName = "Caramel Pecan";
+  }
 }
-
-// function Original(glaze, quantity) {
-//     this.glaze = glaze;
-//     this.quantity = quantity;
-//     this.name = "Original";
-//     this.type = 'original';
-//     this.price = "3.99";
-//     }
-
-// function Blackberry(glaze, quantity) {
-//     this.glaze = glaze;
-//     this.quantity = quantity;
-//     this.name = "Blackberry";
-//     this.type = 'blackberry';
-//     this.price = "3.99";
-//     this.image = "blackberry.png";
-// }
-
-// function Walnut(glaze, quantity) {
-//     this.glaze = glaze;
-//     this.quantity = quantity;
-//     this.name = "Walnut";
-//     this.type = 'walnut';
-//     this.price = "3.99";
-//     this.image = "walnut.png";
-// }
-
-// function Gluten(glaze, quantity) {
-//     this.glaze = glaze;
-//     this.quantity = quantity;
-//     this.name = "Original Gluten-free";
-//     this.type = 'gluten';
-//     this.price = "3.99";
-//     this.image = "gluten-free.png";
-// }
-
-// function Pumpkin(glaze, quantity) {
-//     this.glaze = glaze;
-//     this.quantity = quantity;
-//     this.name = "Pumpkin Spice";
-//     this.type = 'pumpkin';
-//     this.price = "3.99";
-//     this.image = "pumpkin.png";
-// }
-
-// function Pecan(glaze, quantity) {
-//     this.glaze = glaze;
-//     this.quantity = quantity;
-//     this.name = "Caramel Pecan";
-//     this.type = 'pecan';
-//     this.price = "3.99";
-//     this.image = "pecan.png";
-// }
 
 function addToCart(roll) {
   if (localStorage && localStorage.getItem('myCart')) {
+    // cart exists
     var myCart = JSON.parse(localStorage.getItem('myCart'));
     myCart.push(roll);
     localStorage.setItem('myCart', JSON.stringify(myCart));
+  } else {
+    // new item is added to cart
+    var myCart = [roll];
+    localStorage.setItem('myCart', JSON.stringify(myCart));
+    console.log('new item added');
   }
-  console.log('yes');
+  // console.log('yes');
   // console.log(myCart);
 }
 
+function displayCartItems() {
+  if (localStorage && localStorage.getItem('myCart')) {
+    // show cart items
+    var myCart = JSON.parse(localStorage.getItem('myCart'));
+    $.each(myCart, function (key, value) {
+      var roll = value;
+      var flavor = roll.flavor;
+      var glaze = roll.glaze;
+      var quantity = roll.quantity;
+      $('#my-cart').append("<tr class='new-item'> \
+        <td class='cart-image'> \
+          <img src=" + roll.image + "> \
+        </td> \
+        <td class='cart-item'> \
+          <p>" + roll.rollName + "</p> \
+        </td> \
+        <td class='cart-price'> \
+          <p>" + roll.price + "</p> \
+        </td> \
+        <td class='cart-glaze'> \
+          <p>" + roll.glaze + "</p> \
+        </td> \
+        <td class='cart-quantity'> \
+          <p>" + roll.quantity + "</p> \
+        </td> \
+        <td class='cart-item-total'> \
+          <p>" + roll.totalPrice + "</p> \
+        </td> \
+        <td class='remove'><button type='button'>x</button></td> \
+        </tr>")
+    });
+  }
+}
+
+function checkCart() {
+  if (localStorage && localStorage.getItem('myCart')) {
+    if (localStorage.getItem('myCart') == "[]") {
+      $('#my-cart').hide();
+      $('#order-summary').hide();
+      $('.count-bubble').hide();
+    } else {
+        // items exist in cart
+        var myCart = JSON.parse(localStorage.getItem('myCart'));
+        $('.count-bubble').show();
+        $('.count-bubble').text(countItems());
+        $('#no-items').hide();
+        displayCartItems();
+      }
+  } else {
+    // cart has no items (doesnt exist yet)
+    $('#my-cart').hide();
+    $('.count-bubble').hide();
+    $('#order-summary').hide();
+  }
+}
+  
+function countItems() {
+  if (localStorage && localStorage.getItem('myCart')) {
+    if (localStorage.getItem('myCart') == "[]") {
+      // no items in cart
+      return "0";
+    } else {
+      // items exist in cart
+      var myCart = JSON.parse(localStorage.getItem('myCart'));
+      var count = "0";
+      $.each(myCart, function (key, value){
+        var roll = value;
+        var qty = roll.quantity;
+        count = +count + +qty;
+      });
+      console.log(count);
+      return count;
+    }
+  }
+}
+
+
 $(document).ready(function() {
-  var myCart = [];
-  localStorage.setItem('myCart', JSON.stringify(myCart));
 
-  // var glaze = $('input[type=radio][name=glaze]:checked').val();
-  // var quantity = $('select[name=quantity] option:checked').val();
+  checkCart();
 
-  // if (JSON.parse(localStorage.getItem('myCart'),length > 0)) {
-  //   $('#cart').attr('src', 'cart-added.png');
-  // } 
+  $('.remove').click(function() {
+
+    // remove item from myCart in localStorage
+    var myCart = JSON.parse(localStorage.getItem('myCart'));
+    var currRow = $(this).parent();
+    console.log("this row" + currRow);
+    var itemRemoved = $("tr").index(currRow);
+    console.log("tr index " + itemRemoved);
+
+    myCart.splice(itemRemoved-1, 1);
+
+    localStorage.setItem('myCart', JSON.stringify(myCart));
+    console.log(localStorage.getItem('myCart'));
+
+    // remove product listed on cart page
+    $(this).parent().remove();
+    $('.count-bubble').text(countItems());
+
+    if (localStorage.getItem('myCart') == "[]") {
+      $('#my-cart').hide();
+      $('#no-items').show();
+      $('#order-summary').hide();
+      $('.count-bubble').hide();
+    }
+  });
 
   $('input[type=radio][name=glaze]').click(function() {
     var glaze = $('input[type=radio][name=glaze]:checked').val();
@@ -112,19 +174,12 @@ $(document).ready(function() {
   });
 
   $('#add-to-cart').click(function() {
-    console.log('yay');
     var flavor = $('.roll').attr('id');
     var glaze = $('input[type=radio][name=glaze]:checked').val();
     var quantity = $('select[name=quantity] option:checked').val();
 
     var newRoll = new CinnamonRoll(flavor, glaze, quantity);
-    console.log(newRoll);
     addToCart(newRoll);
-    if (JSON.parse(localStorage.getItem('myCart'),length > 0)) {
-      $('#cart').attr('src', 'cart-added.png');
-    }
+    checkCart();
   });
 });
-
-
-
